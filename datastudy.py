1ed2adc51fd3da39d0f2dd02cd8fd8be8d418a1d
alexandre <adang@reputationsquad.com>
2023-07-07 09:53:48 +0200

    simplified data storage in datastudy.py

--- datastudy.py
+++ datastudy.py
@@ -11,20 +11,16 @@ def smallest_index(names: list[str]) -> int:
 class DataStudy:
 
     def __init__(self, name="data_study", desc=""):
-        from framedatafile import FrameDataFile
-        from jsondatafile import JSONDataFile
         self._name = name
         self._desc = desc
-        self.dfs: dict[str, FrameDataFile] = dict()
-        self.jsons: dict[str, JSONDataFile] = dict()
+        self._datas: dict[str, DataStudy] = dict()
         self._views = dict()
 
     def __repr__(self) -> str:
         return (f"DataStudy("
                 f"name={self._name}, "
                 f"desc={self._desc}, "
-                f"dfs={list(self.dfs.values())}, "
-                f"jsons={list(self.jsons.values())})")
+                f"datas={list(self.datas.values())})")
 
     def add_csv(
             self, path, name: str = "", desc: str = "", *args, **kwargs
@@ -32,8 +28,8 @@ class DataStudy:
         from framedatafile import FrameDataFile
 
         if not name:
-            name = f"data_frame_{smallest_index(list(self.dfs.keys()))}"
-        self.dfs[name] = FrameDataFile.from_csv(
+            name = f"data_frame_{smallest_index(list(self.datas.keys()))}"
+        self.datas[name] = FrameDataFile.from_csv(
             self, path, name=name, desc=desc,
             *args, **kwargs
         )
@@ -42,24 +38,12 @@ class DataStudy:
         from jsondatafile import JSONDataFile
 
         if not name:
-            name = f"data_json_{smallest_index(list(self.jsons.keys()))}"
-        self.jsons[name] = JSONDataFile.from_json(self, path, name, desc)
+            name = f"data_json_{smallest_index(list(self.datas.keys()))}"
+        self.datas[name] = JSONDataFile.from_json(self, path, name, desc)
 
     @property
-    def data(self):
-        from datafile import DataFile
-        nb_data = len(self.dfs) + len(self.jsons)
-        if nb_data == 0:
-            raise Exception("DataStudy has no data")
-        elif nb_data == 1:
-            if len(self.dfs) == 1:
-                return list(self.dfs.values())[0]
-            else:
-                return list(self.jsons.values())[0]
-
-        out: dict[str, DataFile] = self.dfs.copy()
-        out.update(self.jsons)
-        return out
+    def datas(self):
+        return self._datas
 
     @property
     def views(self) -> dict[str, Callable]:
